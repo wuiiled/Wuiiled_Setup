b24fa0e4edf2c120e901bb224084182a6f88f1b4
wuiiled <32694740+wuiiled@users.noreply.github.com>
2025-05-21 17:54:18 +0800

    Update convert.sh

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,13 +3,15 @@
 # 函数：生成 ADs_merged.txt
 generate_ads_merged() {
   # 下载并合并规则
-  curl -skL https://a.dove.isdumb.one/pihole.txt >>rules.txt
+  curl -skL https://raw.githubusercontent.com/pmkol/easymosdns/rules/ad_domain_list.txt >>rules.txt
   curl -skL https://raw.githubusercontent.com/wuiiled/Wuiiled_Setup/refs/heads/master/rules/Custom_Reject.list >>rules.txt
   #curl -skL https://adrules.top/adrules_domainset.txt >>rules.txt
-  curl -skL https://small.oisd.nl/domainswild2 >>rules.txt
+  #curl -skL https://small.oisd.nl/domainswild2 >>rules.txt
   #curl -skL https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/reject-list.txt >>rules.txt
   curl -skL https://github.com/TG-Twilight/AWAvenue-Ads-Rule/raw/main/Filters/AWAvenue-Ads-Rule-Surge-RULE-SET.list | sed 's/^DOMAIN,//g' >>rules.txt
   curl -skL https://github.com/limbopro/Adblock4limbo/raw/main/rule/Surge/Adblock4limbo_surge.list | sed 's/^DOMAIN,//g' | sed 's/^DOMAIN-SUFFIX,//g' | sed 's/,reject$//g' >>rules.txt
+  # adobe验证规则
+  curl -skL https://a.dove.isdumb.one/pihole.txt >>rules.txt
 
   # 移除注释和空行
   cat rules.txt | sed '/^#/d' >combined_raw.txt
