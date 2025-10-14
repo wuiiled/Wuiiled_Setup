b89771c305b31e62e2247582391a6b374c25a458
wuiiled <32694740+wuiiled@users.noreply.github.com>
2025-10-14 15:39:52 +0800

    Update convert.sh

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -24,8 +24,8 @@ generate_ads_merged() {
   #curl -skL https://github.com/limbopro/Adblock4limbo/raw/main/rule/Surge/Adblock4limbo_surge.list | sed 's/^DOMAIN,//g' | sed 's/^DOMAIN-SUFFIX,//g' | sed 's/,reject$//g' >>rules.txt
   curl -skL https://ruleset.skk.moe/Clash/domainset/reject.txt | sed 's/+\.//g' >>rules.txt
   echo "" >>rules.txt
-  curl -skL https://ruleset.skk.moe/Clash/domainset/reject_extra.txt | sed 's/+\.//g' >>rules.txt
-  echo "" >>rules.txt
+  #curl -skL https://ruleset.skk.moe/Clash/domainset/reject_extra.txt | sed 's/+\.//g' >>rules.txt
+  #echo "" >>rules.txt
   # adobe验证规则
   curl -skL https://a.dove.isdumb.one/pihole.txt >>rules.txt
   echo "" >>rules.txt
